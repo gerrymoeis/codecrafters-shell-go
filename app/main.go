6d8000803340846c4f9d3ae41dad133b849898f0
gerrymoeis <gerrymoeis@gmail.com>
2026-05-18 09:45:31 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	var command string
-	fmt.Scan(&command)
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	fmt.Print(command, err)
 	fmt.Printf("$ %s: command not found\n", command)
 }
