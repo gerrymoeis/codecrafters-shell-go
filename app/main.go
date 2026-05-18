efa817302f9e0ecd9a100ada4fb1a88bddbd2e73
gerrymoeis <gerrymoeis@gmail.com>
2026-05-18 10:43:25 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -11,12 +11,18 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Print("$ ")
+
 		command, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Printf("there is an error: %v\n", err)
 			break
 		}
 		command = strings.TrimSpace(command)
+
+		if command == "exit" {
+			break
+		}
+
 		fmt.Printf("%s: command not found\n", command)
 	}
 }
